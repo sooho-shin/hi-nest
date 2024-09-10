c6db533125e78636f75e67d05981b6b6ecbdc30e
sooho-shin <soojoon9200@gmail.com>
2024-09-10 10:42:33 +0900

    Update movies.controller.ts

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Delete,
+  Put,
+  Patch,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -7,8 +15,28 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
-  @Get(':id')
+  @Get('/:id')
   getOne(@Param('id') movieId: string) {
     return `This will return movie with id: ${movieId}`;
+  }
+
+  @Post()
+  create() {
+    return 'This will create a new movie';
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: string) {
+    return 'This will delete a movies with ' + movieId;
+  }
+
+  @Put()
+  update() {
+    return 'This will update a movie';
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: string) {
+    return 'This will partially update a movie with' + movieId;
   }
 }
